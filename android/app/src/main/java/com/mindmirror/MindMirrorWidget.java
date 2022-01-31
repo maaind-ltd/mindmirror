6f3a485b830802c3b03b77750149d0f303c64d4d
Elias Rut <eliasrut@gmx.at>
2022-01-31 17:53:27 +0100

    Started implementing the Android widget.

---
+++ android/app/src/main/java/com/mindmirror/MindMirrorWidget.java
@@ -0,0 +1,74 @@
+package com.mindmirror;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.widget.RemoteViews;
+import android.content.SharedPreferences;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Random;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class MindMirrorWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.mind_mirror);
+
+
+        try {
+            SharedPreferences sharedPref = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
+            String appString = sharedPref.getString("appData", "{\"name\":'No measured mood', \"colors\": [214, 214, 214]}");
+            JSONObject appData = new JSONObject(appString);
+            String moodName = appData.getString("name");
+            JSONArray colors = appData.getJSONArray("colors");
+
+            views.setTextViewText(R.id.textView, moodName);
+            int bgColor = Color.argb(255, colors.getInt(0), colors.getInt(1), colors.getInt(2));
+
+            views.setInt(R.id.WidgetLayout, "setBackgroundColor", bgColor);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Intent configIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+
+        views.setOnClickPendingIntent(R.id.WidgetLayout, configPendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
--- src/store/moodSlice.ts
+++ src/store/moodSlice.ts
@@ -1,20 +1,32 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import EmotionState, { EmotionStateWithNone } from '../constants/emotionState';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import EmotionState, {EmotionStateWithNone} from '../constants/emotionState';
+import {NativeModules} from 'react-native';
+import {ColorsRgb} from '../constants/colors';
+const SharedStorage = NativeModules.SharedStorage;
 
 const moodSlice = createSlice({
   name: 'mood',
   initialState: {
     currentMood: EmotionStateWithNone.NoEmotion,
-		targetMood: EmotionState.Flow
+    targetMood: EmotionState.Flow,
   },
   reducers: {
     setCurrentMood: (state, action: PayloadAction<EmotionStateWithNone>) => {
       state.currentMood = action.payload;
+      SharedStorage.set(
+        JSON.stringify({
+          name:
+            action.payload === EmotionStateWithNone.NoEmotion
+              ? 'No measured mood'
+              : action.payload,
+          color: ColorsRgb[action.payload as keyof typeof ColorsRgb],
+        }),
+      );
     },
     setTargetMood: (state, action: PayloadAction<EmotionState>) => {
       state.targetMood = action.payload;
-    }
-  }
+    },
+  },
 });
 
 export default moodSlice;
