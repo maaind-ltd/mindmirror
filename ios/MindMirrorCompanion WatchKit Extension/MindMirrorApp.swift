230b4676b3d159697481899b56a08c0010b402bf
Martin Dinov <martin@maaind.com>
2022-03-17 01:47:47 +0100

    Workout-related code is mostly there. Needs testing on a real watch next to see whether values are updating and coming through

--- ios/MindMirrorCompanion WatchKit Extension/MindMirrorApp.swift
+++ ios/MindMirrorCompanion WatchKit Extension/MindMirrorApp.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 @main
 struct MindMirrorApp: App {
-    @SceneBuilder var body: some Scene {
-        WindowGroup {
-            NavigationView {
-                ContentView()
-            }
-        }
-
-        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
-    }
+  /* Note on @StateObject:
+      SwiftUI’s @StateObject property wrapper is designed to fill a very specific gap in state management: when you need to create a reference type inside one of your views and make sure it stays alive for use in that view and others you share it with.
+   
+   
+   */
+  @StateObject private var workoutManager = WorkoutManager()
+  
+  @SceneBuilder var body: some Scene {
+      WindowGroup {
+          NavigationView {
+              ContentView()
+          }
+          .sheet(isPresented: $workoutManager.showingSummaryView) {
+              SummaryView()
+          }
+          .environmentObject(workoutManager)
+      }
+    WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+  }
 }
