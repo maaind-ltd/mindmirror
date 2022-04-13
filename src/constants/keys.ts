75c905badbeb7f488a006a580d9bdf1d0a6209b8
Elias Rut <eliasrut@gmx.at>
2022-04-13 18:36:45 +0200

    Added code for generating new activation keys.

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -11,3 +11,29 @@ export const isValidActivationKey = (activationKey: string) => {
   }
   return charSum === 620;
 };
+
+// Use this to generate activation keys - they still need to get to the server
+// const possibleCodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// const codes = [];
+// let createCode = () => {
+//   let chars = '';
+//   let charSum = 0;
+//   while (charSum < 620 - 90 || charSum > 620 - 65) {
+//     chars = '';
+//     charSum = 0;
+//     for (let i = 0; i < 7; i++) {
+//       chars += possibleCodes[Math.floor(Math.random() * possibleCodes.length)];
+//       charSum += chars.charCodeAt(i);
+//     }
+//   }
+//   chars += possibleCodes[620 - charSum - 65 /* char code of 'A' */];
+//   charSum += chars.charCodeAt(7);
+//   if (charSum !== 620) {
+//     console.error('Failure while trying to create activation code.');
+//   }
+//   codes.push(chars);
+// };
+
+// for (let i = 0; i <= 500; i++) {
+//   createCode();
+// }
